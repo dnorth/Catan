6821e822508d34a12b083feda160ed49b1d38458
Dallin Cox <dallincox@gmail.com>
2015-09-17 21:16:17 -0600

    Created Server Poller

---
+++ java/src/serverpoller/ServerPoller.java
@@ -0,0 +1,20 @@
+
+public class ServerPoller {
+	
+	int currVersion; //use as an int, or seperate class?
+	//IServer server
+	//Client client
+	
+	ServerPoller(/*IServer, Client*/){} //calls initialize, gets current version, and sends to server
+	
+	void initialize() {} // Begin timer to call server or proxy
+	
+	private
+	boolean newVersion(int newestVersion){ // compare newest version with currVersion - if different, return new
+		return false;
+	}
+	
+	/*statemodeldoc*/ void getNewVersion() {} // update currVersion, receive server model, and return it
+	
+	void sendVersion (/*statemodeldoc*/){} // sends model to server
+}
