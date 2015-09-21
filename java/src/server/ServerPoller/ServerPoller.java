1555d166230be214b2dbbd14c636f6cdd3e9fb9b
Dallin Cox <dallincox@gmail.com>
2015-09-21 09:46:23 -0600

    Generated Javadocs

--- java/src/server/ServerPoller/ServerPoller.java
+++ java/src/server/ServerPoller/ServerPoller.java
@@ -1,21 +1,48 @@
 package server.ServerPoller;
 
+import server.proxy.IServer;
+import client.models.ClientModel;
+
+/**
+ * Polls server periodically to check for changes in model, then receives update <br>
+ */
+
 public class ServerPoller {
 	
-	int currVersion; //use as an int, or seperate class?
-	//IServer server
-	//Client client
+	int currVersion;
+	IServer server; //IServer can be real server or mock proxy
+	ClientModel client;
 	
-	ServerPoller(/*IServer, Client*/){} //calls initialize, gets current version, and sends to server
+	/**
+	 * Constructs ServerPoller, calls initialize
+	 * @param serv pointer to server or mock proxy
+	 * @param cli pointer to client model
+	 */
 	
-	void initialize() {} // Begin timer to call server or proxy
+	ServerPoller(IServer serv, ClientModel cli){} //calls initialize, gets current version, and sends to server
 	
+	/**
+	 * Begins timer to poll server or proxy
+	 */
+	void initialize() {} // Begin timer to poll server or proxy
+	
+	/**
+	 * Stops polling server or proxy
+	 */
+	void stopPolling() {} //Stops polling server or proxy
+	
+	/**
+	 * Compares newest version with currVersion - if different, return true
+	 * @param newestVersion new version number
+	 * @return true if new version different than currVersion
+	 */
 	private
 	boolean newVersion(int newestVersion){ // compare newest version with currVersion - if different, return new
 		return false;
 	}
 	
-	/*statemodeldoc*/ void getNewVersion() {} // update currVersion, receive server model, and return it
-	
-	void sendVersion (/*statemodeldoc*/){} // sends model to server
+	/**
+	 * Updates currVersion, receives server's model, and sends to client
+	 */
+	void getNewVersion() {} // update currVersion, receive server model, and send it to client model
 }
