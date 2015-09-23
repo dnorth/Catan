96aeb1865ec66445cc87bd183fdfb08439c4b8ec
Dallin Cox <dallincox@gmail.com>
2015-09-23 12:17:18 -0600

    comment in Facade

--- java/src/client/facade/Facade.java
+++ java/src/client/facade/Facade.java
@@ -3,6 +3,9 @@ package client.facade;
 import server.proxy.ClientCommunicator;
 import client.models.ClientModel;
 
+/**
+ * Facade class interfaces from GUI to Client Communicator
+ */
 public class Facade {
 	private PlayerManager playerManager;
 	private TurnManager turnManager;
