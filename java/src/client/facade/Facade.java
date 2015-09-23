90a20170713652365bd47175aca296ec0aa1db5b
Dallin Cox <dallincox@gmail.com>
2015-09-22 22:28:37 -0600

    Small Facade update

--- java/src/client/facade/Facade.java
+++ java/src/client/facade/Facade.java
@@ -1,5 +1,15 @@
 package client.facade;
 
+import server.proxy.ClientCommunicator;
+import client.models.ClientModel;
+
 public class Facade {
 	private PlayerManager playerManager;
+	private TurnManager turnManager;
+	private ClientModel client;
+	private ClientCommunicator clientCommunicator;
+	
+	public Facade (ClientModel cli) {
+		
+	}
 }
