a7eb7d9faa401de62309d9ea62ce6d4f7e8623a6
Seth Jackson <sethmjackson@gmail.com>
2015-09-28 12:09:34 -0600

    Implemented rollDice()

--- java/src/client/facade/TurnManager.java
+++ java/src/client/facade/TurnManager.java
@@ -1,5 +1,9 @@
 package client.facade;
 
+import java.util.Random;
+
+import client.models.mapdata.Hex;
+
 public class TurnManager 
 {
 
@@ -7,39 +11,53 @@ public class TurnManager
 	 * rolls the dice.
 	 * @return a random number between 2 and 12.
 	 */
-	public int rollDice(){return 1;}
-	
+	public int rollDice()
+	{
+		Random rand = new Random();
+
+		int  die1 = rand.nextInt(6) + 1;
+		int  die2 = rand.nextInt(6) + 1;
+		return die1+die2;
+	}
+
 	/**
 	 * delivers resources to players based on dice roll, calls moveRobber() and robPlayerCards() if the number is a 7.
 	 * @param diceRoll
 	 */
-	public void getResources(int diceRoll){}
-	
+	public void getResources(int diceRoll)
+	{
+
+	}
+
 	/**
 	 * moves the robber and steals a card from one of the surrounding players.
 	 * @param playerIndex
 	 */
-	public void moveRobber(int playerIndex){}
-	
+	public void moveRobber(int playerIndex)
+	{
+
+
+	}
+
 	/**
 	 * removes half of the cards of any player that has more than 7.
 	 */
 	public void robPlayerCards(){}
-	
+
 	/**
 	 * initiates trade between two players.
 	 * @param playerIndex1 
 	 * @param playerIndex2
 	 */
 	public void startTradeOfferWithPlayer(int playerIndex1, int playerIndex2){}
-	
+
 	/**
 	 * accepts or rejects trade offer.
 	 * @param playerIndex1
 	 * @param playerIndex2
 	 */
 	public void finishTradeOfferWithPlayer(int playerIndex1, int playerIndex2){}
-	
+
 	/**
 	 * trade 4 resource cards with bank for any 1 resource or maritime trade.
 	 * @param playerIndex
@@ -56,64 +74,64 @@ public class TurnManager
 	 * @param playerIndex
 	 */
 	public void buildRoad(int playerIndex){}
-	
+
 	/**
 	 * builds settlement on a hex vertex.
 	 * @param playerIndex
 	 */
 	public void buildSettlement(int playerIndex){}
-	
+
 	/**
 	 * upgrades settlement to city.
 	 * @param playerIndex
 	 */
 	public void upgradeSettlement(int playerIndex){}
-	
+
 	/**
 	 * buys development card for player.
 	 * @param playerIndex
 	 */
 	public void buyDevCard(int playerIndex){}
-	
+
 	/**
 	 * chooses which dev card to use.
 	 * @param playerIndex
 	 */
 	public void playDevCard(int playerIndex){}
-	
+
 	/**
 	 * gives player two resource cards of their choice.
 	 * @param playerIndex
 	 */
 	public void playYearOfPlenty(int playerIndex){}
-	
+
 	/**
 	 * moves robber, but doesn't call robPlayerCards.
 	 * @param playerIndex
 	 */
 	public void playSoldier(int playerIndex){moveRobber(playerIndex);}
-	
+
 	/**
 	 * gives all of the selected resource to the player.
 	 * @param playerIndex
 	 */
 	public void playMonopoly(int playerIndex){}
-	
+
 	/**
 	 * adds monument to player.
 	 * @param playerIndex
 	 */
 	public void playMonument(int playerIndex){}
-	
+
 	/**
 	 * builds two roads for player.
 	 * @param playerIndex
 	 */
 	public void playRoadBuilding(int playerIndex){buildRoad(playerIndex); buildRoad(playerIndex);}
-	
+
 	/**
 	 * ends player's turn.
 	 */
 	public void endTurn(){}
-	
+
 }
