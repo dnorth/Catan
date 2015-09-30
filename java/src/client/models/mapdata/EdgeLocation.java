76f7e48e7fa2290baec625dc2e7c812f4ebfa3ad
Dallin Cox <dallincox@gmail.com>
2015-09-29 21:56:23 -0600

    EdgeLocation stuff

--- java/src/client/models/mapdata/EdgeLocation.java
+++ java/src/client/models/mapdata/EdgeLocation.java
@@ -9,4 +9,24 @@ public class EdgeLocation {
 	int xcoord;
 	int ycoord;
 	String direction;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EdgeLocation other = (EdgeLocation) obj;
+		if (direction == null) {
+			if (other.direction != null)
+				return false;
+		} else if (!direction.equals(other.direction))
+			return false;
+		if (xcoord != other.xcoord)
+			return false;
+		if (ycoord != other.ycoord)
+			return false;
+		return true;
+	}
 }
