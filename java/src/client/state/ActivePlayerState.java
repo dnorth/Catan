3ec0db59760afaea8f12d3e09406ed665f8c3dad
Thomas Williams <tkwilli94@gmail.com>
2015-10-13 21:42:39 -0600

    IStateBase defined and stubs added for all implementing state classes

--- java/src/client/state/ActivePlayerState.java
+++ java/src/client/state/ActivePlayerState.java
@@ -1,5 +1,108 @@
 package client.state;
 
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+import client.models.TradeOffer;
+
 public class ActivePlayerState implements IStateBase {
+
+	@Override
+	public boolean canJoinGame() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canStartCreateNewGame() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canStartPlayerWaitingView() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canLogin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canBuyRoad(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceRoadAtLocation(int playerIndex, EdgeLocation edge) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canBuySettlement(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceSettlementAtLocation(int playerIndex,
+			VertexLocation vertLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canUpgradeSettlement(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canUpgradeSettlementAtLocation(int playerIndex,
+			VertexLocation vertLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceRobber(HexLocation hexLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canOfferTrade(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canAcceptTrade(int playerIndex, TradeOffer tradeOffer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canMaritimeTrade(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canBuyDevCard(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlayDevCard(int playerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- java/src/client/state/IStateBase.java
+++ java/src/client/state/IStateBase.java
@@ -1,13 +1,9 @@
 package client.state;
 
-import shared.definitions.CatanColor;
-import shared.definitions.PieceType;
-import shared.definitions.ResourceType;
 import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
 import shared.locations.VertexLocation;
-import client.data.GameInfo;
-import client.data.RobPlayerInfo;
+
 import client.models.TradeOffer;
 
 interface IStateBase {
@@ -16,17 +12,17 @@ interface IStateBase {
 	public boolean canStartCreateNewGame();
 	public boolean canStartPlayerWaitingView();
 	public boolean canLogin();
-	public boolean canPlaceSettlement(VertexLocation vertLoc);
-	public boolean canPlaceCity(VertexLocation vertLoc);
-	public boolean canPlaceRobber(HexLocation hexLoc);
-	
 	public boolean canBuyRoad(int playerIndex);
 	public boolean canPlaceRoadAtLocation(int playerIndex, EdgeLocation edge);
+	public boolean canBuySettlement(int playerIndex);
+	public boolean canPlaceSettlementAtLocation(int playerIndex, VertexLocation vertLoc);
+	public boolean canUpgradeSettlement(int playerIndex);
+	public boolean canUpgradeSettlementAtLocation(int playerIndex, VertexLocation vertLoc);
+	public boolean canPlaceRobber(HexLocation hexLoc);
+	
 	public boolean canOfferTrade(int playerIndex);
 	public boolean canAcceptTrade(int playerIndex, TradeOffer tradeOffer);
 	public boolean canMaritimeTrade(int playerIndex);
-	public boolean canBuySettlement(int playerIndex);
-	public boolean canUpgradeSettlement(int playerIndex);
 	public boolean canBuyDevCard(int playerIndex);
 	public boolean canPlayDevCard(int playerIndex);
 }
