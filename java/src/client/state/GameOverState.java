f6669f4c676cf3028e4f416f32aa0a6fa9320ed8
Thomas Williams <tkwilli94@gmail.com>
2015-10-13 22:23:01 -0600

    finalized states

--- java/src/client/state/GameOverState.java
+++ java/src/client/state/GameOverState.java
@@ -13,70 +13,59 @@ public class GameOverState extends IStateBase {
 		return false;
 	}
 
-	@Override
+	@Override //Is this right??
 	public boolean canStartCreateNewGame() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+	@Override //Is this right??
 	public boolean canStartPlayerWaitingView() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canLogin() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 
 	@Override
 	public boolean canPlaceRoadAtLocation(EdgeLocation edge) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canPlaceSettlementAtLocation(VertexLocation vertLoc) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canUpgradeSettlementAtLocation(VertexLocation vertLoc) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canPlaceRobber(HexLocation hexLoc) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canOfferTrade(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canAcceptTrade(int playerIndex, TradeOffer tradeOffer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canBuyDevCard(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canPlayDevCard(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
--- java/src/client/state/SetupOneActivePlayerState.java
+++ java/src/client/state/SetupOneActivePlayerState.java
@@ -9,74 +9,62 @@ public class SetupOneActivePlayerState extends IStateBase {
 
 	@Override
 	public boolean canJoinGame() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canStartCreateNewGame() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canStartPlayerWaitingView() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canLogin() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	@Override
 	public boolean canPlaceRoadAtLocation(EdgeLocation edge) {
-		// TODO Auto-generated method stub
-		return false;
+		return facade.canPlaceRoad(edge);
 	}
 
 	@Override
 	public boolean canPlaceSettlementAtLocation(VertexLocation vertLoc) {
-		// TODO Auto-generated method stub
-		return false;
+		return facade.canPlaceSettlement(vertLoc);
 	}
 
 	@Override
 	public boolean canUpgradeSettlementAtLocation(VertexLocation vertLoc) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 
 	@Override
 	public boolean canPlaceRobber(HexLocation hexLoc) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canOfferTrade(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canAcceptTrade(int playerIndex, TradeOffer tradeOffer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canBuyDevCard(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canPlayDevCard(int playerIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
