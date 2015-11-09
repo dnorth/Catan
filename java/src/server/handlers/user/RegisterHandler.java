5bffb36a67541841d64fd74b521ca595dfc130ec
Danny North <dnorth2@hotmail.com>
2015-11-09 12:22:47 -0700

    Register returns 'Success' although it doesn't do anything. this is just a simple example of server response.

--- java/src/server/handlers/user/RegisterHandler.java
+++ java/src/server/handlers/user/RegisterHandler.java
@@ -1,8 +1,10 @@
 package server.handlers.user;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.logging.Logger;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -17,10 +19,15 @@ public class RegisterHandler implements HttpHandler {
 	 * @see com.sun.net.httpserver.HttpHandler#handle(com.sun.net.httpserver.HttpExchange)
 	 */
 	@Override
-	public void handle(HttpExchange arg0) throws IOException {
+	public void handle(HttpExchange exchange) throws IOException {
 		// TODO Auto-generated method stub
 		logger = Logger.getLogger("Catan");
-
+		logger.info("Handling Register.");
+		exchange.getResponseHeaders().set("Content-Type", "application/json");
+		String response = "Success";
+		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length());
+		exchange.getResponseBody().write(response.getBytes());
+        exchange.close();
 	}
 
 }
