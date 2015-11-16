0af709ae43fba8202273424adfb6f10a735786a5
Seth Jackson <sethmjackson@gmail.com>
2015-11-16 12:14:21 -0700

    Refactored MovesFacade to have gameIndex as a parameter for all methods.

--- java/src/server/facade/MovesFacade.java
+++ java/src/server/facade/MovesFacade.java
@@ -69,7 +69,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/Year_of_Plenty
 	 */
 	@Override
-	public ClientModel playYearOfPlenty(String type, int gameID, int playerIndex, ResourceType resource1, ResourceType resource2) {
+	public ClientModel playYearOfPlenty(String type, int gameIndex, int playerIndex, ResourceType resource1, ResourceType resource2) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -87,7 +87,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/Soldier
 	 */
 	@Override
-	public ClientModel playSoldier(String type, int playerIndex, int victimIndex, HexLocation location) {
+	public ClientModel playSoldier(String type, int gameIndex, int playerIndex, int victimIndex, HexLocation location) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -96,7 +96,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/Monopoly
 	 */
 	@Override
-	public ClientModel playMonopoly(String type, ResourceType resource, int playerIndex) {
+	public ClientModel playMonopoly(String type, ResourceType resource, int gameIndex, int playerIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -105,7 +105,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/Monument
 	 */
 	@Override
-	public ClientModel playMonument(String type, int playerIndex) {
+	public ClientModel playMonument(String type, int gameIndex, int playerIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -114,7 +114,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/buildRoad
 	 */
 	@Override
-	public ClientModel buildRoad(String type, int playerIndex, EdgeLocation roadLocation, boolean free) {
+	public ClientModel buildRoad(String type, int gameIndex, int playerIndex, EdgeLocation roadLocation, boolean free) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -122,7 +122,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/buildCity
 	 */
 	@Override
-	public ClientModel buildCity(String type, int playerIndex, VertexLocation vertexLocation) {
+	public ClientModel buildCity(String type, int gameIndex, int playerIndex, VertexLocation vertexLocation) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -131,7 +131,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/buildSettlement
 	 */
 	@Override
-	public ClientModel buildSettlement(String type, int playerIndex, VertexLocation vertexLocation, boolean free) {
+	public ClientModel buildSettlement(String type, int gameIndex, int playerIndex, VertexLocation vertexLocation, boolean free) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -141,7 +141,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/offerTrade
 	 */
 	@Override
-	public ClientModel offerTrade(String type, int playerIndex, Resources offer, int receiver) {
+	public ClientModel offerTrade(String type, int gameIndex, int playerIndex, Resources offer, int receiver) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -150,7 +150,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/acceptTrade
 	 */
 	@Override
-	public ClientModel acceptTrade(String type, int playerIndex, boolean willAccept) {
+	public ClientModel acceptTrade(String type, int gameIndex, int playerIndex, boolean willAccept) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -159,8 +159,8 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/maritimeTrade
 	 */
 	@Override
-	public ClientModel maritimeTrade(String type, int playerIndex, int ratio, String inputResource,
-			String outputResource) {
+	public ClientModel maritimeTrade(String type, int gameIndex, int playerIndex, int ratio,
+			String inputResource, String outputResource) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -169,7 +169,7 @@ public class MovesFacade implements iMovesFacade {
 	 * Facade for the command moves/discardCards
 	 */
 	@Override
-	public ClientModel discardCards(String type, int playerIndex, Resources discardedCards) {
+	public ClientModel discardCards(String type, int gameIndex, int playerIndex, Resources discardedCards) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- java/src/server/facade/iMovesFacade.java
+++ java/src/server/facade/iMovesFacade.java
@@ -95,104 +95,114 @@ public interface iMovesFacade {
 	 * Play soldier.
 	 *
 	 * @param type "Soldier"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the person playing the dev card
 	 * @param victimIndex the victim index
 	 * @param location the hex location of the robber being moved to
 	 * @return the client model
 	 */
-	public ClientModel playSoldier(String type, int playerIndex, int victimIndex, HexLocation location);
+	public ClientModel playSoldier(String type, int gameIndex, int playerIndex, int victimIndex, HexLocation location);
 	
 	/**
 	 * Play monopoly.
 	 *
 	 * @param type "Monopoly"
 	 * @param resource the resource of the person playing the dev card
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index
 	 * @return the client model
 	 */
-	public ClientModel playMonopoly(String type, ResourceType resource, int playerIndex);
+	public ClientModel playMonopoly(String type, ResourceType resource, int gameIndex, int playerIndex);
 	
 	/**
 	 * Play monument.
 	 *
 	 * @param type "Monument"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the person playing the dev card
 	 * @return the client model
 	 */
-	public ClientModel playMonument(String type, int playerIndex);
+	public ClientModel playMonument(String type, int gameIndex, int playerIndex);
 	
 	/**
 	 * Builds the road.
 	 *
 	 * @param type "buildRoad"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the person building the road
 	 * @param roadLocation the Edge Location where the road is located
 	 * @param free whether or not the road is built for free
 	 * @return the client model
 	 */
-	public ClientModel buildRoad(String type, int playerIndex, EdgeLocation roadLocation, boolean free);
+	public ClientModel buildRoad(String type, int gameIndex, int playerIndex, EdgeLocation roadLocation, boolean free);
 	
 	/**
 	 * Builds the settlement.
 	 *
 	 * @param type "buildSettlement"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the person building the settlement
 	 * @param vertexLocation the vertex location where the settlement is placed
 	 * @param free whether or no the settlement is built for free
 	 * @return the client model
 	 */
-	public ClientModel buildSettlement(String type, int playerIndex, VertexLocation vertexLocation, boolean free);
+	public ClientModel buildSettlement(String type, int gameIndex, int playerIndex, VertexLocation vertexLocation, boolean free);
 	
 	/**
 	 * Builds the city.
 	 *
 	 * @param type "buildCity"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the person building the city
 	 * @param vertexLocation the vertex location where the city will be placed
 	 * @return the client model
 	 */
-	public ClientModel buildCity(String type, int playerIndex, VertexLocation vertexLocation);
+	public ClientModel buildCity(String type, int gameIndex, int playerIndex, VertexLocation vertexLocation);
 	
 	/**
 	 * Offer trade.
 	 *
 	 * @param type "offerTrade"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index 
 	 * @param offer the offer
 	 * @param receiver the receiver
 	 * @return the client model
 	 */
-	public ClientModel offerTrade(String type, int playerIndex, Resources offer, int receiver);
+	public ClientModel offerTrade(String type, int gameIndex, int playerIndex, Resources offer, int receiver);
 	
 	/**
 	 * Accept trade.
 	 *
 	 * @param type "acceptTrade"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the player accepting the trade
 	 * @param willAccept whether or not the player accepts the trade
 	 * @return the client model
 	 */
-	public ClientModel acceptTrade(String type, int playerIndex, boolean willAccept);
+	public ClientModel acceptTrade(String type, int gameIndex, int playerIndex, boolean willAccept);
 	
 	/**
 	 * Maritime trade.
 	 *
 	 * @param type "maritimeTrade"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of the player offering the trade
 	 * @param ratio the ratio (Optional) of resources to trade
 	 * @param inputResource the resource type that is being traded
 	 * @param outputResource the resource type that is being received 
 	 * @return the client model
 	 */
-	public ClientModel maritimeTrade(String type, int playerIndex, int ratio, String inputResource, String outputResource);
+	public ClientModel maritimeTrade(String type, int gameIndex, int playerIndex, int ratio, String inputResource, String outputResource);
 	
 	/**
 	 * Discard cards.
 	 *
 	 * @param type "discardCards"
+	 * @param gameIndex TODO
 	 * @param playerIndex the player index of ther person discarding
 	 * @param discardedCards the resource list of the discarded cards
 	 * @return the client model
 	 */
-	public ClientModel discardCards(String type, int playerIndex, Resources discardedCards);
+	public ClientModel discardCards(String type, int gameIndex, int playerIndex, Resources discardedCards);
 }
