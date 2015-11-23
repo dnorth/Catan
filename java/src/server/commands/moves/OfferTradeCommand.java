fdf4610deca315cd6d02502f50c4245eb3fa3b7d
Danny North <dnorth2@hotmail.com>
2015-11-23 13:08:03 -0700

    Took out statement requiring player to have the proper resources in order for the trade to go through. Trade should always go through

--- java/src/server/commands/moves/OfferTradeCommand.java
+++ java/src/server/commands/moves/OfferTradeCommand.java
@@ -4,6 +4,7 @@ import client.models.ClientModel;
 import client.models.Player;
 import client.models.Resources;
 import client.models.TradeOffer;
+import client.models.TurnTracker;
 import client.models.communication.MessageLine;
 import server.commands.IMovesCommand;
 import server.exceptions.InvalidPlayerIndexException;
@@ -44,6 +45,7 @@ public class OfferTradeCommand implements IMovesCommand {
 	public void execute() throws InvalidStatusException, NotYourTurnException, InvalidPlayerIndexException {
 
 		Player offerer = game.getClientModel().getPlayers()[playerIndex];
+		TurnTracker t = game.getClientModel().getTurnTracker();
 
 		ClientModel model = game.getClientModel();
 		model.checkStatus("Playing");
@@ -51,9 +53,9 @@ public class OfferTradeCommand implements IMovesCommand {
 		model.checkPlayerIndex(playerIndex);
 		
 		TradeOffer tradeOffer = new TradeOffer(playerIndex, receiver, offer);
-		if(offerer.hasSpecifiedResources(tradeOffer.reverseOffer())){
-			game.getClientModel().setTradeOffer(tradeOffer);
-		}
+		
+		game.getClientModel().setTradeOffer(tradeOffer);
+		
 		
 		game.getClientModel().getLog().getLines().add(new MessageLine(offerer.getName() + " offered a trade", offerer.getName()));
 		game.getClientModel().increaseVersion();
