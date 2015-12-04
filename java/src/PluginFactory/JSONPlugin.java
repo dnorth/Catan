bcf1e510da09c88bb2399d8cbea346070b6ddc51
tgrossen <tgrossen@gmail.com>
2015-12-03 21:00:35 -0700

    Add class variables, class functions to JSONPlugin and SQLPlugin.

--- java/src/PluginFactory/JSONPlugin.java
+++ java/src/PluginFactory/JSONPlugin.java
@@ -1,5 +1,91 @@
 package PluginFactory;
 
+import server.commands.IMovesCommand;
+import server.model.ServerData;
+import server.model.ServerGame;
+import server.model.ServerUser;
+import JSONDAO.*;
+
+import com.google.gson.JsonObject;
+
 public class JSONPlugin extends IPlugin {
 
+	private ServerData serverData;
+	private GameJSONDAO gameDAO;
+	private UserJSONDAO userDAO;
+	private CommandJSONDAO commandDAO;
+	private ModelJSONDAO modelDAO;
+	
+	@Override
+	public JsonObject loadUsers() {
+		// TODO Auto-generated method stub
+		return super.loadUsers();
+	}
+
+	@Override
+	public JsonObject loadGames() {
+		// TODO Auto-generated method stub
+		return super.loadGames();
+	}
+
+	@Override
+	public void saveUsers(ServerUser[] users) {
+		// TODO Auto-generated method stub
+		super.saveUsers(users);
+	}
+
+	@Override
+	public void saveGame(ServerGame game) {
+		// TODO Auto-generated method stub
+		super.saveGame(game);
+	}
+
+	@Override
+	public void saveCommand(ServerGame game, IMovesCommand command) {
+		// TODO Auto-generated method stub
+		super.saveCommand(game, command);
+	}
+
+	public ServerData getServerData() {
+		return serverData;
+	}
+
+	public void setServerData(ServerData serverData) {
+		this.serverData = serverData;
+	}
+
+	public GameJSONDAO getGameDAO() {
+		return gameDAO;
+	}
+
+	public void setGameDAO(GameJSONDAO gameDAO) {
+		this.gameDAO = gameDAO;
+	}
+
+	public UserJSONDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public void setUserDAO(UserJSONDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public CommandJSONDAO getCommandDAO() {
+		return commandDAO;
+	}
+
+	public void setCommandDAO(CommandJSONDAO commandDAO) {
+		this.commandDAO = commandDAO;
+	}
+
+	public ModelJSONDAO getModelDAO() {
+		return modelDAO;
+	}
+
+	public void setModelDAO(ModelJSONDAO modelDAO) {
+		this.modelDAO = modelDAO;
+	}
+
+	
+	
 }
--- java/src/PluginFactory/SQLPlugin.java
+++ java/src/PluginFactory/SQLPlugin.java
@@ -1,5 +1,89 @@
 package PluginFactory;
 
+import server.commands.IMovesCommand;
+import server.model.ServerData;
+import server.model.ServerGame;
+import server.model.ServerUser;
+import SQLDAO.*;
+
+import com.google.gson.JsonObject;
+
 public class SQLPlugin extends IPlugin {
+
+	private ServerData serverData;
+	private GameSQLDAO gameDAO;
+	private UserSQLDAO userDAO;
+	private CommandSQLDAO commandDAO;
+	private GameUserMapSQLDAO gameUserMapDAO;
+	
+	@Override
+	public JsonObject loadUsers() {
+		// TODO Auto-generated method stub
+		return super.loadUsers();
+	}
+
+	@Override
+	public JsonObject loadGames() {
+		// TODO Auto-generated method stub
+		return super.loadGames();
+	}
+
+	@Override
+	public void saveUsers(ServerUser[] users) {
+		// TODO Auto-generated method stub
+		super.saveUsers(users);
+	}
+
+	@Override
+	public void saveGame(ServerGame game) {
+		// TODO Auto-generated method stub
+		super.saveGame(game);
+	}
+
+	@Override
+	public void saveCommand(ServerGame game, IMovesCommand command) {
+		// TODO Auto-generated method stub
+		super.saveCommand(game, command);
+	}
+
+	public ServerData getServerData() {
+		return serverData;
+	}
+
+	public void setServerData(ServerData serverData) {
+		this.serverData = serverData;
+	}
+
+	public GameSQLDAO getGameDAO() {
+		return gameDAO;
+	}
+
+	public void setGameDAO(GameSQLDAO gameDAO) {
+		this.gameDAO = gameDAO;
+	}
+
+	public UserSQLDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public void setUserDAO(UserSQLDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public CommandSQLDAO getCommandDAO() {
+		return commandDAO;
+	}
+
+	public void setCommandDAO(CommandSQLDAO commandDAO) {
+		this.commandDAO = commandDAO;
+	}
+
+	public GameUserMapSQLDAO getGameUserMapDAO() {
+		return gameUserMapDAO;
+	}
+
+	public void setGameUserMapDAO(GameUserMapSQLDAO gameUserMapDAO) {
+		this.gameUserMapDAO = gameUserMapDAO;
+	}
 
 }
