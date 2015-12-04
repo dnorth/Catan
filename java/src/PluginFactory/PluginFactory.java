2f93c24af5f1590d71767e9afb01e5047fe08f77
Dallin Cox <dallincox@gmail.com>
2015-12-03 21:51:22 -0700

    Implemented PluginFactory (hopefully) #kk

--- java/src/PluginFactory/PluginFactory.java
+++ java/src/PluginFactory/PluginFactory.java
@@ -1,9 +1,24 @@
 package PluginFactory;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class PluginFactory {
 	
-	public static IPlugin CreatePlugin(String arg) {
-		return null;	
+	public IPlugin CreatePlugin(String arg) {
+		String jarFilePath = "Plugins" + File.separator + arg + ".jar";
+		try {
+			URL jarURL = new URL(jarFilePath);
+			URLClassLoader child = new URLClassLoader(new URL[] {jarURL}, this.getClass().getClassLoader());
+			Class classToLoad = Class.forName(arg + "Plugin", true, child);
+			IPlugin plugin = (IPlugin)classToLoad.newInstance(); //if errors here, try getConstructor().newInstance()
+			return plugin;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 }
